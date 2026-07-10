84966b78f1322be0b6e61ccbba62ea750d1150b6
Dylan Tanoh <dylan.tanoh166@gmail.com>
2026-07-10 15:36:05 +0000

    correction start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ set -e
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
-PHP artisan db:seed --force
+php artisan db:seed --force
 
 php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
